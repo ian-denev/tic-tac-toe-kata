8bea0a45d8a1ce5e16a89588d0936c34494644f7
Ian Denev <contact@iandenev.com>
2021-11-04 00:03:29 +0000

    test: argument is required

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const solveTicTacToe = (...data) => {
-    
+const solveTicTacToe = (gameSequence) => {
+    if (gameSequence === undefined) throw new Error("an argument is required");
 }
 
 module.exports = { solveTicTacToe }
--- index.test.js
+++ index.test.js
@@ -1,1 +1,8 @@
 const { solveTicTacToe } = require("./index");
+
+describe("solveTicTacToe testing suite", () => {
+    test("gameSequence is required", () => {
+        expect(() => solveTicTacToe()).toThrow("an argument is required");
+        expect(() => solveTicTacToe(3)).not.toThrow("an argument is required");
+    });
+})
