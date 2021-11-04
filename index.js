a10e6d65d850927905475f14277cbfc1435c6224
Ian Denev <contact@iandenev.com>
2021-11-04 03:52:18 +0000

    test: added multiple input tests for gameSequence

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const solveTicTacToe = (gameSequence) => {
     if (gameSequence === undefined) throw new Error("an argument is required");
+    else if (!Array.isArray(gameSequence)) throw new Error("gameSequence must be an Array");
+    else if (gameSequence.length == 0) throw new Error("gameSequence cannot be empty");
+    else if (gameSequence.length > 9) throw new Error("gameSequence must contain at most 9 events");
+    gameSequence.forEach((el, index) => {
+        if (el.length != 3) throw new Error("gameSequence elements must follow the correct format");
+        else if (typeof el[0] != "string") throw new Error("gameSequence elements must follow the correct format");
+        else if (typeof el[1] != "number") throw new Error("gameSequence elements must follow the correct format");
+        else if (typeof el[2] != "number") throw new Error("gameSequence elements must follow the correct format");
+        else if (el[0] != "O" && el[0] != "X") throw new Error("gameSequence elements must follow the correct format");
+        else if (el[1] < 0 || el[1] > 2) throw new Error("gameSequence elements must follow the correct format");
+        else if (el[2] < 0 || el[2] > 2) throw new Error("gameSequence elements must follow the correct format");
+        else if (gameSequence[index + 1]) {
+            if (el[0] == gameSequence[index + 1][0]) throw new Error("players must alternate");
+        }
+    })
 }
 
 module.exports = { solveTicTacToe }
--- index.test.js
+++ index.test.js
@@ -1,8 +1,46 @@
 const { solveTicTacToe } = require("./index");
 
 describe("solveTicTacToe testing suite", () => {
+    // Dummy Objects
+    const gameSequence = [
+        ["X", 1, 1],
+        ["O", 2, 2],
+        ["X", 0, 1],
+        ["O", 0, 0],
+        ["X", 2, 1]
+    ];
+
+    const gameSequenceBadElements = [
+        { sequence: [["O", 1, 2], ["X", 1, 0, 2]] },
+        { sequence: [["O", 1, 1], ["X", 77, 0]] },
+        { sequence: [["X", -1, 1], ["O", 2, 0]] }
+    ];
+
     test("gameSequence is required", () => {
         expect(() => solveTicTacToe()).toThrow("an argument is required");
         expect(() => solveTicTacToe(3)).not.toThrow("an argument is required");
+    });
+
+    test("gameSequence must be an Array", () => {
+        expect(() => solveTicTacToe("str")).toThrow("gameSequence must be an Array");
+        expect(() => solveTicTacToe(gameSequence)).not.toThrow("gameSequence must be an Array");
+    });
+
+    test("gameSequence cannot be empty", () => {
+        expect(() => solveTicTacToe([])).toThrow("gameSequence cannot be empty");
+    });
+
+    test("gameSequence must contain at most 9 events", () => {
+        const overflowingGameSeq = [[], [], [], [], [], [], [], [], [], [], [], [], []];
+        expect(() => solveTicTacToe(overflowingGameSeq)).toThrow("gameSequence must contain at most 9 events");
+    });
+
+    test.each(gameSequenceBadElements)("gameSequence elements follow the correct input format", (badSequence) => {
+        expect(() => solveTicTacToe(badSequence.sequence)).toThrow("gameSequence elements must follow the correct format");
+    });
+
+    test("players must alternate", () => {
+        const nonAlternatingGameSeq = [["X", 1, 1], ["X", 0, 0]];
+        expect(() => solveTicTacToe(nonAlternatingGameSeq)).toThrow("players must alternate");
     });
 })
